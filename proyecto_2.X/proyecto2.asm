9343a21faea3e594363fd3a47d7a06dd6089d6cf
mor19213 <68123853+mor19213@users.noreply.github.com>
2020-10-23 23:41:45 -0600

    Add files via upload
    usando interrupciones

--- proyecto_2.X/proyecto2.asm
+++ proyecto_2.X/proyecto2.asm
@@ -13,7 +13,7 @@
 ;				VARIABLES
 ;****************************************************************************
 GPR_VAR	    UDATA
-VALOR_ADC   RES 1
+RECIBIDO   RES 1
 NIBBLE_H    RES 1
 NIBBLE_L    RES 1
 BANDERAS    RES 1
@@ -22,7 +22,12 @@ VAR_STATUS  RES 1
 TIEMPO_1    RES 1
 TIEMPO_2    RES 1
 ENVIAR	    RES 1
-	    
+DISPLAY_1   RES 1
+DISPLAY_2   RES 1
+DISPLAY_3   RES 1
+DISPLAY_4   RES 1
+   
+   
 	    
 ;				INTERRUPCIONES
 ;*******************************************************************************
@@ -41,10 +46,12 @@ PUSH:
 ISR:
     BTFSC   INTCON, T0IF
     CALL    FUE_TMR0
-    BTFSC   PIR1, TXIF
-    CALL    COSO_TX
+    BTFSC   PIR1, TMR2IF
+    CALL    FUE_TMR2
     BTFSC   PIR1, ADIF
     CALL    COSO_ADC
+    BTFSC   PIR1, RCIF
+    CALL    COSO_RX
     
 POP:
     SWAPF   VAR_STATUS, W
@@ -56,15 +63,35 @@ POP:
 ;		    SUB-RUTINAS DE LA INTERRUPCION 
 ;*******************************************************************************
 
+FUE_TMR2:
+    BTFSC   PIR1, TXIF
+    CALL    COSO_TX
+    BCF	    PIR1, TMR2IF
+    RETURN
+   
 FUE_TMR0:
     MOVLW   .248
     MOVWF   TMR0
     CALL    DISPLAYS
     BCF	    INTCON, T0IF 
+    RETURN
 
+COSO_ADC:
+   BCF	    PIR1, ADIF	    ;BANDERA TERMINAR CONVERSION
+   MOVFW    ADRESH
+   MOVWF    ENVIAR	    ;VALOR DE ADRESH A VARIABLE ENVIAR
+   BSF	    ADCON0, 1
+   RETURN
+   
+COSO_TX:
+    MOVFW   ENVIAR
+    MOVWF   TXREG
     RETURN
     
-    
+COSO_RX:
+    MOVFW   RCREG
+    MOVWF   PORTD
+    RETURN
 ;				TABLA
 ;******************************************************************************* 
 
@@ -98,48 +125,22 @@ MAIN_PROG   CODE
 
 START
    CALL	    CONFIG_IO
-   CALL	    CONFIG_TMR0		    ; 2 mS
+   CALL	    CONFIG_TMR		    ; 2 mS
    CALL	    CONFIG_INTERRUPT
    CALL	    CONFIG_OSC
    CALL	    CONFIG_ADC
    CALL	    CONFIG_SERIAL
    GOTO	    LOOP
    
-LOOP
-   BSF	    ADCON0, GO	    ;CONVERSION
-   BTFSC    ADCON0, GO
-   GOTO	    $-1
-   BCF	    PIR1, ADIF	    ;BANDERA TERMINAR CONVERSION
-   MOVFW    ADRESH
-   MOVWF    ENVIAR	    ;VALOR DE ADRESH A VARIABLE ENVIAR
-   
-   ;ENVIAR LOS VALORES AL PUERTO D Y A LOS DISPLAYS
-   BTFSS    PIR1, RCIF	    
-   GOTO	    RECIBIDO  
-   MOVFW    RCREG
-   MOVWF    VALOR_ADC
-   MOVFW    RCREG
-   MOVWF    PORTD
-   CALL	    CONTADOR
-   GOTO	    LOOP
-   
-RECIBIDO
-   BTFSS    PIR1, TXIF
-   GOTO	    LOOP
-   MOVFW    ENVIAR
-   MOVWF    TXREG
-   GOTO	    LOOP ;;;
-   
-CONTADOR
-    ;SEPARAR NIBBLES
-    CLRF    NIBBLE_L
-    CLRF    NIBBLE_H
-    MOVFW   VALOR_ADC
+LOOP:
+    CLRF    DISPLAY_1
+    CLRF    DISPLAY_2
+    MOVFW   PORTD
     ANDLW   B'00001111'
-    MOVWF   NIBBLE_L
-    SWAPF   VALOR_ADC, W
+    MOVWF   DISPLAY_1
+    SWAPF   PORTD, W
     ANDLW   B'00001111'
-    MOVWF   NIBBLE_H
+    MOVWF   DISPLAY_2
     GOTO    LOOP
    
 ;*******************************************************************************
@@ -150,7 +151,7 @@ DISPLAYS:
     GOTO    DISPLAY2
 DISPLAY1
     CLRF    PORTE
-    MOVFW   NIBBLE_H
+    MOVFW   DISPLAY_1
     CALL    TABLA
     MOVWF   PORTB
     BSF	    PORTE, RE0
@@ -160,15 +161,13 @@ DISPLAY1
 
 DISPLAY2
     CLRF    PORTE
-    MOVFW   NIBBLE_L
+    MOVFW   DISPLAY_2
     CALL    TABLA
     MOVWF   PORTB
     BSF	    PORTE, RE1
     CLRF    BANDERAS
     BSF	    BANDERAS, 0
     RETURN
-    
-
 
 ;				CONFIGURACIONES
 ;*******************************************************************************
@@ -176,7 +175,7 @@ DISPLAY2
 CONFIG_IO
    BANKSEL  ANSEL
    CLRF	    ANSEL
-   COMF	    ANSEL
+   BSF	    ANSEL, 0
    BANKSEL  TRISA
    CLRF	    TRISA
    COMF	    TRISA
@@ -196,31 +195,38 @@ CONFIG_OSC
    MOVFW    OSCCON
    RETURN
    
-CONFIG_ADC
+CONFIG_ADC:
    BANKSEL  ADCON1
    CLRF	    ADCON1
    BANKSEL  ADCON0
-   MOVLW    B'01000001'
+   MOVLW    B'10000011'
    MOVWF    ADCON0
    RETURN
    
 
-CONFIG_TMR0			    ; 2 mS PARA DISPLAYS
+CONFIG_TMR			    ; 2 mS PARA DISPLAYS
     BANKSEL OPTION_REG
     CLRWDT
     MOVLW   b'01010111'
-    MOVWF   OPTION_REG
+    MOVWF   OPTION_REG			    ; 1 SEGUNDO PARA LEDS
+    BANKSEL PORTA
+    MOVLW   .255
+    MOVWF   T2CON
     RETURN
 
     
-CONFIG_INTERRUPT
-    ; CUANDO SE PRENDE LA BANDERA HABRA UNA INTERRUPCION TIMER0 
-    ;BSF	    INTCON, GIE
-    ; INTERRUPCION TIMER 1 Y 2
+CONFIG_INTERRUPT:
     BANKSEL TRISA
+    BSF	    PIE1, ADIE
     BSF	    PIE1, TMR1IE
+    BSF	    PIE1, TMR2IE
+    BSF	    PIE1, RCIE
+    BSF	    PIE1, TXIE
     BSF	    INTCON, T0IE
     BSF	    INTCON, PEIE
+    
+    MOVLW   .20
+    MOVWF   PR2
     
     BANKSEL PORTA
     BSF	    INTCON, GIE
@@ -245,6 +251,5 @@ CONFIG_SERIAL
     BSF	    TXSTA, TXEN
     BANKSEL PORTA
     RETURN
-   
-   
+
 END
